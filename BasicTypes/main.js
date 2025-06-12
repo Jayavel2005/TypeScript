4d248fcb87de478e68323e5542fd141e4b0662d3
Jayavel <jayavel1520@gmail.com>
2025-06-12 22:16:19 +0530

    basic Data Types in TS

---
+++ BasicTypes/main.js
@@ -0,0 +1,28 @@
+// Basic Data Types
+// Number
+var num1 = 10;
+var num2 = 20;
+console.log(num1, num2);
+// Strings
+var userName = "Jayavel";
+console.log(userName);
+// Boolean
+var isDeveloper = true;
+// Null and Undefined
+var x = undefined;
+var y = null;
+// Any
+var value = "Jayavel"; // Any can store any kind of data type
+// Practice
+var Name = "Jayavel";
+var age = 20;
+var isDev = true;
+var me = "Only Dev";
+var college = "Rathinam Technical campus"; // ❌ Fix this
+var num = 100;
+var isPassed = num > 35;
+console.log(isPassed);
+var myName = "Jayavle";
+var myAge = 20;
+var isStudent = true;
+console.log("Hi, my name is ".concat(myName, ". I'm ").concat(myAge, " years old. Am I a student? ").concat(isStudent));
---
+++ BasicTypes/main.ts
@@ -0,0 +1,55 @@
+// Basic Data Types
+
+// Number
+
+let num1 : number = 10;
+let num2 : number = 20;
+
+console.log(num1, num2);
+
+
+// Strings
+
+let userName: string = "Jayavel";
+
+console.log(userName);
+
+
+// Boolean
+
+let isDeveloper:boolean = true;
+
+
+// Null and Undefined
+
+let x:undefined = undefined;
+
+let y:null = null;
+
+// Any
+
+let value:any = "Jayavel"  // Any can store any kind of data type
+
+
+// Practice
+
+let Name:string = "Jayavel";
+let age:number = 20;
+let isDev: boolean = true;
+let me:any = "Only Dev";
+
+
+let college: string = "Rathinam Technical campus";  // ❌ Fix this
+
+
+let num: number = 100;
+let isPassed: boolean = num > 35;
+console.log(isPassed);
+
+
+let myName: string = "Jayavle";
+let myAge: number = 20;
+let isStudent: boolean = true;
+
+console.log(`Hi, my name is ${myName}. I'm ${myAge} years old. Am I a student? ${isStudent}`);
+
